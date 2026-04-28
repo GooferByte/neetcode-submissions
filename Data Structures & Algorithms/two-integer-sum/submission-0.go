47301871313a24541d176f473910d4a69792ad82
Devanshu Srivastava <84028771+GooferByte@users.noreply.github.com>
2026-04-28 15:28:20 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    
+	m := make(map[int]int)
+	for i, v := range(nums){
+		if idx, ok :=m[target-v]; ok{
+			return []int{idx, i}
+		} else {
+			m[v]=i
+		}
+	}
+	return nil
+}
