a3fcf59786995ace701211bbbadd237c24411070
Devanshu Srivastava <84028771+GooferByte@users.noreply.github.com>
2026-07-15 15:49:07 +0530

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.go
@@ -0,0 +1,20 @@
+func longestConsecutive(nums []int) int {
+	if len(nums)==0 {return 0}
+	set := make(map[int]bool)
+	max :=1
+	for _, i := range(nums){
+		set[i]=true
+	}
+	for _, i := range(nums){
+		curr := i
+		cnt := 1
+		for set[curr+1]{
+			cnt++
+			curr++
+		}
+		if (cnt>max){
+			max = cnt
+		} 
+	}
+	return max
+}
